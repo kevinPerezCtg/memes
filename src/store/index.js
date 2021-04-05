eb512eabd1be5889489a096970c034533595404f
kevinPerezCtg <kevin.daniel.1995@hotmail.com>
2021-04-05 17:48:06 -0500

    app de memes

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,39 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    titleApp: "Memes",
+    memes: [],
   },
-  mutations: {
+  mutations: {   
+    setMemes(state,payload){
+      state.memes = payload;
+    }, 
   },
   actions: {
+    async getMemes({commit}, params){
+      try {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        const result = await response.json();
+
+        console.log(result.data.memes)
+        console.log(params)
+        if(!params?.total){
+          commit("setMemes", result.data.memes);
+        }else{
+          const resultTemp = [];
+          result.data.memes.forEach((meme, index) => {
+            if(index < params.total) resultTemp.push(meme);
+          });
+          commit("setMemes", resultTemp);
+        }                
+      } catch (error) {
+        console.log("error ",error);
+      }
+    }
   },
   modules: {
+  },
+  getters: {
+
   }
 })
